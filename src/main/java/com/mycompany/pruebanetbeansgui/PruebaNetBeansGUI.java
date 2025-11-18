c9755e712c0957efa78172b58d36e6e08bc33c8f
UlisesChacon <uliseschacon96@gmail.com>
2025-11-18 11:02:54 -0500

    uwu

--- src/main/java/com/mycompany/pruebanetbeansgui/PruebaNetBeansGUI.java
+++ src/main/java/com/mycompany/pruebanetbeansgui/PruebaNetBeansGUI.java
@@ -11,6 +11,6 @@ package com.mycompany.pruebanetbeansgui;
 public class PruebaNetBeansGUI {
 
     public static void main(String[] args) {
-        System.out.println("Hello World GUI!");
+        System.out.println("Hello World GUI UWU!");
     }
 }
